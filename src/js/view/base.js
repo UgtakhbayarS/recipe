a3c2f8e89c279d40e5d9dc9c9f91485aadc2d186
ugtakhbayar <ugtkahbayar0217@gmail.com>
2020-01-15 21:06:35 +0800

    add loader

---
+++ src/js/view/base.js
@@ -0,0 +1,26 @@
+export const elements = {
+  searchForm: document.querySelector(".search"),
+  searchInput: document.querySelector(".search__field"),
+  searchResultList: document.querySelector(".results__list"),
+  searchResultsLoader: document.querySelector(".results")
+};
+
+export const ElementString = {
+  loader: "loader"
+};
+
+export const clearRenderLoader = () => {
+  const loader = document.querySelector(`.${ElementString.loader}`);
+  if (loader) loader.parentElement.removeChild(loader);
+};
+
+export const renderLoader = parent => {
+  const iconLoader = `
+    <div class="${ElementString.loader}">
+      <svg>
+        <use href="img/icons.svg#icon-cw"</use>
+        </svg>
+    </div>`;
+
+  parent.insertAdjacentHTML("afterbegin", iconLoader);
+};
